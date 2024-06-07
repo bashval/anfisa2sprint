555c61aa12dec2e3638955f103c47e50a26e5511
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-06-07 09:57:46 +0200

    First changes

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Topping, Wrapper, IceCream
+from homepage.models import TestModel
+
+
+admin.site.register(Category)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+admin.site.register(TestModel)
+admin.site.register(IceCream)
